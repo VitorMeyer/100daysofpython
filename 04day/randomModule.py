083bcf817fb933789fb6be290bad6ae008cb4df3
Vitor Meyer <vitor.e.s.meyer@gmail.com>
2026-06-02 20:46:02 -0300

    day 04 init.

---
+++ 04day/randomModule.py
@@ -0,0 +1,8 @@
+import random
+
+randomInteger = random.randint(1,10)
+
+print(f"number is: {randomInteger}")
+
+randomNumber0To1 = random.random() * 10
+print (randomNumber0To1)
---
+++ 04day/whowillpaythebill.py
@@ -0,0 +1,9 @@
+import random
+friends = ["Alice", "Bruno", "Carla", "Daniel", "Eduarda", "Felipe"]
+
+randomFriend = random.randint(0,5)
+
+print(f"pay the bill, {friends[randomFriend]}!")
+
+print(f"pay the bill,{random.choice(friends)}")
+
